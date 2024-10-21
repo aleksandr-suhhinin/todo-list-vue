3b5c2410dbc4ddd025b1edc434be68d7e0594672
aleksandr-suhhinin <asuhhinin@gmail.com>
2024-10-21 10:52:34 +0300

    removed unnecessary dependencies

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { VueDragula } from 'vue3-dragula';
-// import 'vue3-dragula/dist/dragula.css';
 const app = createApp(App)
 
 app.use(createPinia())
